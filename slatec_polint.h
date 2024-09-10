a6000aaa801ae324f699cde8a01cb9c544c2695d
Roy Ratcliffe <roy@ratcliffe.me>
2024-09-10 07:12:29 +0100

    feat: polyinterp.h

--- slatec_polint.h
+++ slatec_polint.h
@@ -8,27 +8,26 @@
 /*!
  * \brief Double-precision polynomial interpolation.
  *
- * \details Function \c polint generates a polynomial that interpolates the
- * vectors \c x[i] by \c y[i] where \c i ranges from 1 to \c{n}. It prepares
+ * \details Function \c slatec_polint generates a polynomial that interpolates
+ * the vectors \c x[i] by \c y[i] where \c i ranges from 1 to \c{n}. It prepares
  * information in the array \c c that can be utilized by the subroutine \c
  * polyvl to calculate the polynomial and its derivatives.
  *
  * \see https://netlib.org/slatec/src/polint.f
  */
-static inline int polint(size_t n, const double x[], const double y[],
-                         double c[]) {
+static inline int slatec_polint(size_t n, const double x[], const double y[],
+                                double c[]) {
   if (n == 0)
-    return 1;
+    return -1;
   c[0] = y[0];
   if (n == 1)
     return 0;
-  size_t i = n;
-  for (size_t k = 1; k < i; k++) {
+  for (size_t k = 1; k < n; k++) {
     c[k] = y[k];
     for (size_t i = 0; i < k; i++) {
       const double dif = x[i] - x[k];
       if (dif == 0)
-        return 2;
+        return -2;
       c[k] = (c[i] - c[k]) / dif;
     }
   }
@@ -38,20 +37,19 @@ static inline int polint(size_t n, const double x[], const double y[],
 /*!
  * \brief Single-precision polynomial interpolation.
  */
-static inline int polintf(size_t n, const float x[], const float y[],
-                          float c[]) {
+static inline int slatec_polintf(size_t n, const float x[], const float y[],
+                                 float c[]) {
   if (n == 0)
-    return 1;
+    return -1;
   c[0] = y[0];
   if (n == 1)
     return 0;
-  size_t i = n;
-  for (size_t k = 1; k < i; k++) {
+  for (size_t k = 1; k < n; k++) {
     c[k] = y[k];
-    for (size_t i = 0; i < k; i++) {
+    for (size_t i = 0; i < k - 1; i++) {
       const float dif = x[i] - x[k];
       if (dif == 0)
-        return 2;
+        return -2;
       c[k] = (c[i] - c[k]) / dif;
     }
   }
--- slatec_polyvl.h
+++ slatec_polyvl.h
@@ -11,41 +11,51 @@
  * \details Calculates the value of a polynomial where the polynomial was
  * produced by a previous call to \c{polint}. The argument \c n and the arrays
  * \c x and \c c must not be altered between the call to \c polint and the call
- * to \c{polyvl}.
+ * to \c{slatec_polyvl}.
+ *
+ * Simplifies the original Fortran.
  *
  * \see https://netlib.org/slatec/src/polyvl.f
  */
-static inline double polyvl(double xx, size_t n, const double x[],
-                            const double c[]) {
+static inline int slatec_polyvl(double xx, double *yy, size_t n,
+                                const double x[], const double c[]) {
+  if (n == 0)
+    return -1;
   double pione = 1, pone = c[0];
-  if (n == 1)
-    return pone;
+  if (n == 1) {
+    *yy = pone;
+    return 0;
+  }
   double ptwo;
   for (size_t k = 1; k < n; k++) {
     const double pitwo = (xx - x[k - 1]) * pione;
     pione = pitwo;
-    ptwo = pone = pitwo * c[k];
+    ptwo = pone + pitwo * c[k];
     pone = ptwo;
   }
-  return ptwo;
+  *yy = ptwo;
+  return 0;
 }
 
 /*!
  * \brief Computes polynomial single-precision values.
  */
-static inline float polyvlf(float xx, size_t n, const float x[],
-                            const float c[]) {
+static inline int slatec_polyvlf(float xx, float *yy, size_t n, const float x[],
+                                 const float c[]) {
   if (n == 0)
-    return 0;
+    return -1;
   float pione = 1, pone = c[0];
-  if (n == 1)
-    return pone;
+  if (n == 1) {
+    *yy = pone;
+    return 0;
+  }
   float ptwo;
   for (size_t k = 1; k < n; k++) {
     const float pitwo = (xx - x[k - 1]) * pione;
     pione = pitwo;
-    ptwo = pone = pitwo * c[k];
+    ptwo = pone + pitwo * c[k];
     pone = ptwo;
   }
-  return ptwo;
+  *yy = ptwo;
+  return 0;
 }
